d83c1fa872e0d2b58b3d17c08e87b3d7a307e940
anuragKumarCB <anuragkumar.cb01@gmail.com>
2023-12-08 20:09:13 +0530

    complete project

--- 02counterProject/src/App.jsx
+++ 02counterProject/src/App.jsx
@@ -1,33 +1,48 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // let count = 10;
+  let upperLimit = 20;
+  let lowerLimit = 0;
+  let [count, setCount] = useState(10)
+
+  const addCount = () => {
+    if (count < upperLimit) {
+      count = count + 1;
+      console.log(count)
+      setCount(count)
+    }
+  }
+
+  const removeCount = () => {
+    if (count > lowerLimit) {
+      count = count - 1;
+      console.log(count)
+      setCount(count)
+    }
+
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Counter App</h1>
+      <h3>Count : {count}</h3>
+      <br />
+      <button
+        onClick={addCount}
+      >
+        Increase Count
+      </button>
+      <button
+        onClick={removeCount}
+      >
+        Decrease Count
+      </button>
+      <p>I have count value too : {count}</p>
+      <span>well i have too : {count}</span>
+      <h4>no sh*t : {count}</h4>
+      <h3>do whatever you want but you cant go upper than <span className='limits'>{upperLimit}</span> and lower than <span className='limits'>{lowerLimit}</span></h3>
     </>
   )
 }
