52a9c3e7a6512fd96c89994d69d421cca921bb62
anuragKumarCB <anuragkumar.cb01@gmail.com>
2023-12-10 09:59:54 +0530

    removed commented functions

--- 01basic/04backgroundChanger/src/App.jsx
+++ 01basic/04backgroundChanger/src/App.jsx
@@ -1,46 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 import ColorBtn from './components/ColorBtn'
-import ColorBtnData from './components/ColorBtnData'
+// import ColorBtnData from './components/ColorBtnData'
 
 function App() {
   //intial color using state
   const [bgColor, setBgColor] = useState('black')
 
-  // function for changing color
-  // const setRed = () => {
-  //   setColor(prevColor => 'red')
-  // }
-  // const setGreen = () => {
-  //   setColor(prevColor => 'green')
-  // }
-  // const setYellow = () => {
-  //   setColor(prevColor => 'yellow')
-  // }
-  // const setBlue = () => {
-  //   setColor(prevColor => 'blue')
-  // }
-  // const setOlive = () => {
-  //   setColor(prevColor => 'olive')
-  // }
-  // const setGray = () => {
-  //   setColor(prevColor => 'gray')
-  // }
-  // const setPink = () => {
-  //   setColor(prevColor => 'pink')
-  // }
-  // const setPurple = () => {
-  //   setColor(prevColor => 'purple')
-  // }
-  // const setLarvender = () => {
-  //   setColor(prevColor => 'larvender')
-  // }
-  // const setWhite = () => {
-  //   setColor(prevColor => 'white')
-  // }
-  // const setBlack = () => {
-  //   setColor(prevColor => 'black')
-  // }
   const cName = 'py-2 px-6 border-2 rounded-full';
   return (
     <div className='h-screen w-screen ' style={{ backgroundColor: bgColor }}>
