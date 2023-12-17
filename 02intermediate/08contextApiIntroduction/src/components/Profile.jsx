7f534eac2d46a0cd7b9ab85fe9a2a290298f9037
anuragKumarCB <anuragkumar.cb01@gmail.com>
2023-12-17 11:12:04 +0530

    updated code in profile.jsx

--- 02intermediate/08contextApiIntroduction/src/components/Profile.jsx
+++ 02intermediate/08contextApiIntroduction/src/components/Profile.jsx
@@ -1,8 +1,6 @@
 import React, { useContext } from 'react'
 import UserContext from '../context/UserContext'
 
-
-
 const Profile = () => {
     //now to use context data we need to extract it
     const { user } = useContext(UserContext)
